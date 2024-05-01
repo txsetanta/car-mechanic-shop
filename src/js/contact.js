bf7d3b9d4f9d318a95626b6975a5983cbfd9e2ba
Saqib Islam <saaqi.grw@gmail.com>
2024-05-01 13:23:07 +0500

    minor

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,59 +1,101 @@
-// Store status
-function checkStoreStatus() {
-  // Set the timezone to Orange City, Florida, USA
+// // Store status
+function storeStatus() {
   const timeZone = "America/New_York";
   const now = new Date();
-  const orangeCityTime = new Date(
-    now.toLocaleString("en-US", { timeZone: timeZone })
+  const localTime = new Date(
+    now.toLocaleString("en-US", {
+      timeZone: timeZone
+    })
   );
+  // Days ---------------------------
+  const options = { weekday: "long" };
+  const formattedTime = now.toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: timeZone,
+  });
+  const orangeCityDayToday = new Intl.DateTimeFormat("en-US", options).format(localTime);
+  const outputDay = `${orangeCityDayToday} ${formattedTime}`;
 
-  const dayOfWeek = orangeCityTime.toLocaleString("en-US", {
+  // Time --------------------------
+  const hours = localTime.getHours();
+  const dayOfWeek = localTime.toLocaleString("en-US", {
     timeZone,
     weekday: "long",
   });
-  const hours = orangeCityTime.getHours();
-
   const isOpeningHours =
     (dayOfWeek === "Sunday" && hours >= 9 && hours < 17) ||
     (dayOfWeek !== "Sunday" && hours >= 8 && hours < 19);
 
-  return isOpeningHours
+  const outputTime = isOpeningHours
     ? '<span class="open text-success fw-bold">Open</span> come on down'
-    : '<span class="closed text-danger fw-bold">Closed</span> see you in the morning';
-}
-function detectDayToday() {
-  const currentDate = new Date();
-  const options = { weekday: "long" };
-  const dayToday = new Intl.DateTimeFormat("en-US", options).format(
-    currentDate
-  );
+    : '<span class="closed text-danger fw-bold">Closed</span> see you tomorrow. 🙂';
 
-  const orangeCityTimeZone = "America/New_York"; // Time zone identifier for Orange City, Florida
-  const formattedTime = currentDate.toLocaleTimeString("en-US", {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-    timeZone: orangeCityTimeZone,
-  });
-
-  const orangeCityDate = new Date(
-    currentDate.toLocaleString("en-US", { timeZone: orangeCityTimeZone })
-  );
-  const orangeCityDayToday = new Intl.DateTimeFormat("en-US", options).format(
-    orangeCityDate
-  );
-
-  return `${orangeCityDayToday} ${formattedTime}`;
+  return `${outputDay}, We are ${outputTime}`;
 }
 
 function updateStatusRealtime() {
-  document.getElementById(
-    "storestatus"
-  ).innerHTML = `${detectDayToday()}, We Are ${checkStoreStatus()}`;
+  document.getElementById("storestatus").innerHTML =
+    `${storeStatus()}`;
 }
-
 updateStatusRealtime();
 setInterval(updateStatusRealtime, 15000);
+// function checkStoreStatus() {
+//   // Set the timezone to Orange City, Florida, USA
+//   const timeZone = "America/New_York";
+//   const now = new Date();
+//   const orangeCityTime = new Date(
+//     now.toLocaleString("en-US", { timeZone: timeZone })
+//   );
+
+//   const dayOfWeek = orangeCityTime.toLocaleString("en-US", {
+//     timeZone,
+//     weekday: "long",
+//   });
+//   const hours = orangeCityTime.getHours();
+
+//   const isOpeningHours =
+//     (dayOfWeek === "Sunday" && hours >= 9 && hours < 17) ||
+//     (dayOfWeek !== "Sunday" && hours >= 8 && hours < 19);
+
+//   return isOpeningHours
+//     ? '<span class="open text-success fw-bold">Open</span> come on down'
+//     : '<span class="closed text-danger fw-bold">Closed</span> see you in the morning';
+// }
+// function detectDayToday() {
+//   const currentDate = new Date();
+//   const options = { weekday: "long" };
+//   const dayToday = new Intl.DateTimeFormat("en-US", options).format(
+//     currentDate
+//   );
+
+//   const orangeCityTimeZone = "America/New_York"; // Time zone identifier for Orange City, Florida
+//   const formattedTime = currentDate.toLocaleTimeString("en-US", {
+//     hour: "numeric",
+//     minute: "2-digit",
+//     hour12: true,
+//     timeZone: orangeCityTimeZone,
+//   });
+
+//   const orangeCityDate = new Date(
+//     currentDate.toLocaleString("en-US", { timeZone: orangeCityTimeZone })
+//   );
+//   const orangeCityDayToday = new Intl.DateTimeFormat("en-US", options).format(
+//     orangeCityDate
+//   );
+
+//   return `${orangeCityDayToday} ${formattedTime}`;
+// }
+
+// function updateStatusRealtime() {
+//   document.getElementById(
+//     "storestatus"
+//   ).innerHTML = `${detectDayToday()}, We Are ${checkStoreStatus()}`;
+// }
+
+// updateStatusRealtime();
+// setInterval(updateStatusRealtime, 15000);
 
 
 
